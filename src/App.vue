437cb980488c6543885cffb4ce45c2192c9a732d
Bobby Pinard <bobby.pinard@gmail.com>
2018-07-15 20:52:51 -0500

    Passing data to compounded component.

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,38 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+
+    <form action="">
+      <label for="ii">Initial Investment</label>
+      <input v-model="ii" type="number" id="ii" /><br />
+      <label for="compounded">Compounded</label>
+      <input v-model="time" type="number" id="time" /><br />
+      <input type="submit" text="Submit" />
+      {{ ii * time }}
+    </form>
+
+    <compound Cii="ii" Ctime="time" />
   </div>
 </template>
 
 <script>
+import Compound from "./components/Compound"
+
 export default {
+  components: {
+    Compound
+  },
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      ii: 0,
+      time: 0
     }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+input {
+  width: 5em;
 }
 </style>
---
+++ src/components/Compound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="compound">
+    <h1>Compound Working</h1>
+    {{ this.Cii }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    Cii: Number,
+    Ctime: Number
+  }
+}
+</script>
